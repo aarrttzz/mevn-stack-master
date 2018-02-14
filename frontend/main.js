39cfd1545c458af5ccfdf3cb94d73cbcd893725d
aarrttzz <arty_mail@bk.ru>
2018-02-14 20:53:57 +0300

    init

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,12 @@
 "use strict";
-require('./index/main');
+
 
 if (window.location.hash.indexOf('article')!=-1){
     require.ensure([],function(require){
        require('./article/main');
+    });
+}else{
+    require.ensure([],function(require){
+        require('./index/main');
     });
 }

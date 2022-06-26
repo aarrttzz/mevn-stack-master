c70bfa105c6f8189a7123afd3daa11094904f9bb
Artem Pichugin <arty_mail@bk.ru>
2022-06-27 00:28:22 +0300

    remember

--- frontend/admin/config/http.js
+++ frontend/admin/config/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://artempichugin.herokuapp.com/api', //base URL goes here
+  baseURL: 'http://localhost:4000/api'// 'https://artempichugin.herokuapp.com/api', //base URL goes here
 })
--- frontend/main.js
+++ frontend/main.js
@@ -3,12 +3,12 @@
 var md5 = require('md5');
 var md5hash = md5(window.location.hash);
 
-if (md5hash.indexOf('72c03f1d37bbe365aadaec12640e76a0')!=-1){
+if (window.location.hash.indexOf('admin')!=-1){
     require.ensure([],function(require){
        require('./article/main');
     });
 }else{
-    require.ensure([],function(require){
+    require.ensure([], function (require) {
         require('./index/main');
     });
 }

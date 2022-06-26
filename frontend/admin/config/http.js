6f25c2557ebbd36b79935078a36cf74d3852008b
Artem Pichugin <arty_mail@bk.ru>
2022-06-27 00:55:15 +0300

    deploy

--- frontend/admin/config/http.js
+++ frontend/admin/config/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:4000/api'// 'https://artempichugin.herokuapp.com/api', //base URL goes here
+  baseURL: 'https://https://artempichugin.netlify.app/api', //base URL goes here
 })

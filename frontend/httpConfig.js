89f7e1eabeb6de7354361d101f0aba043f7ba11e
Artem Pichugin <arty_mail@bk.ru>
2022-07-06 20:37:23 +0300

    localhost

--- frontend/httpConfig.js
+++ frontend/httpConfig.js
@@ -1,4 +1,4 @@
 
 // export default 'https://artempichugin.herokuapp.com/api/';
-// export default 'http://localhost:80/api/';
-export default 'https://artempichugin.herokuapp.com/api'
+export default 'http://localhost:80/api/';
+// export default 'https://artempichugin.herokuapp.com/api'
